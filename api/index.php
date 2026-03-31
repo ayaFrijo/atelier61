7f935af1835b024244f614de6d30a925f00583c0
unknown <Yoyoaya56367@gmail.com>
2026-03-31 17:33:49 +0100

    first commit

---
+++ api/index.php
@@ -0,0 +1,8 @@
+<?php 
+echo "<h1>Bonjour depuis PHP sur Vercel</h1>"; 
+echo "<p>Déploiement réussi.</p>"; 
+echo"<a href="exercice1.php">exercice1</a>";
+echo"<a href="exercice2.php">exercice2</a>";
+echo"<a href="/Rapport Exercice1.pdf">Rapport-exercice 1</a>";
+echo"<a href="/Rapport Exercice2.pdf">Rapport-exercice 2</a>";
+?> 
