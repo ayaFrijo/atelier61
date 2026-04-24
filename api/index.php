048fa6ddde3e2081e2c5fcd0d458ddd022249731
unknown <Yoyoaya56367@gmail.com>
2026-04-24 11:14:33 +0100

    atelier7

--- api/index.php
+++ api/index.php
@@ -26,7 +26,8 @@ $workshops = [
     ["title" => "Exercice 10", "atelier" => "6", "code" => "https://github.com/ayaFrijo/atelier61.git", "pdf" => "Rapport Exercice101.pdf"],
     ["title" => "Exercice 11", "atelier" => "6", "code" => "https://github.com/ayaFrijo/atelier61.git", "pdf" => "Rapport Exercice111.pdf"],
     ["title" => "Exercice 12", "atelier" => "6", "code" => "https://github.com/ayaFrijo/atelier61.git", "pdf" => "Rapport Exercice121.pdf"],
-    ["title" => "Exercice 13", "atelier" => "6", "code" => "https://github.com/ayaFrijo/atelier61.git", "pdf" => "Rapport Exercice131.pdf"]
+    ["title" => "Exercice 13", "atelier" => "6", "code" => "https://github.com/ayaFrijo/atelier61.git", "pdf" => "Rapport Exercice131.pdf"],
+    ["title" => "Exercice 14", "atelier" => "7", "code" => "https://github.com/ayaFrijo/atelier7.git", "pdf" => "Rapport Exercice141.pdf"]
 
 
 ];
