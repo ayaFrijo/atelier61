a16bb90b3badd2f26882296016cc1da161e56fa3
unknown <Yoyoaya56367@gmail.com>
2026-05-15 11:03:34 +0100

    ff

--- api/index.php
+++ api/index.php
@@ -30,8 +30,8 @@ $workshops = [
     ["title" => "Exercice 14", "atelier" => "7", "code" => "https://github.com/ayaFrijo/atelier7.git", "pdf" => "Rapport Exercice141.pdf"],
     ["title" => "Exercice 15", "atelier" => "8", "code" => "https://github.com/ayaFrijo/atelier8.git", "pdf" => "Rapport Exercice151.pdf"],
     ["title" => "bag store", "atelier" => "store", "code" => "https://github.com/ayaFrijo/bag_store.git", "pdf" => "Rapport de store.pdf"],
-    ["title" => "Exercice 16", "atelier" => "9", "code" => "https://github.com/ayaFrijo/atelier9.git", "pdf" => "Rapport Exercice16.pdf"]
-
+    ["title" => "Exercice 16", "atelier" => "9", "code" => "https://github.com/ayaFrijo/atelier9.git", "pdf" => "Rapport Exercice16.pdf"],
+    ["title" => "Exercice 17", "atelier" => "10", "code" => "https://github.com/ayaFrijo/atelier10.git", "pdf" => "Rapport Exercice17.pdf"]
 
 
 ];
